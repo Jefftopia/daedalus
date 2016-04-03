4a6cc40fb071593f683feeaaf6353d716b573697
Jeff <jsmith6690@gmail.com>
2016-04-03 21:20:36 +0000

    refactor /js; being web work

---
+++ js/web/daedalus/boot.ts
@@ -0,0 +1,6 @@
+import 'core-js/client/shim';
+import 'angular2/bundles/angular2-polyfills';
+import { bootstrap } from 'angular2/platform/browser';
+import { Daedalus } from './daedalus';
+
+bootstrap(Daedalus, []);
---
+++ js/web/daedalus/daedalus.ts
@@ -0,0 +1,3 @@
+import { Component, OnInit, AfterViewInit } from 'angular2/core';
+import { COMMON_DIRECTIVES } from 'angular2/common';
+
