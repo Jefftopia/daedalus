d51e2a224a49ca959fd725e23dd3f4f65acb6657
Jeff <jsmith6690@gmail.com>
2016-04-04 01:21:06 +0000

    add some A2 boilerplate, begin writing classes for website

--- js/web/daedalus/boot.ts
+++ js/web/daedalus/boot.ts
@@ -1,6 +1,11 @@
 import 'core-js/client/shim';
 import 'angular2/bundles/angular2-polyfills';
+import 'angular2/bundles/http';
+import 'angular2/bundles/router';
 import { bootstrap } from 'angular2/platform/browser';
+import { ROUTER_PROVIDERS } from 'angular2/router';
 import { Daedalus } from './daedalus';
 
-bootstrap(Daedalus, []);
+bootstrap(Daedalus, [
+    ROUTER_PROVIDERS
+    ]);
--- js/web/daedalus/daedalus.ts
+++ js/web/daedalus/daedalus.ts
@@ -1,3 +1,41 @@
 import { Component, OnInit, AfterViewInit } from 'angular2/core';
 import { COMMON_DIRECTIVES } from 'angular2/common';
+import { RouteConfig, ROUTER_DIRECTIVES, Router } from 'angular2/router';
 
+@Component({
+    selector: 'daedalus'
+    templateUrl: './daedalus.html',
+    providers: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+    { path: '/home',                   name: 'Home',         component: MatchesView },
+    { path: '/matches',                name: 'Matches',      component: MatchesView },
+    { path: '/match-details/:matchId', name: 'MatchDetails', component: MatchDetailsView }
+])
+export class Daedalus {
+    
+    protected router: Router;
+    
+    constructor(router: Router) {
+        this.router = router;
+    }
+    
+    ngOnInit(): void {
+        
+    }
+    
+}
+/*
+@RouteConfig([
+
+  { // Crisis Center child route
+    path: '/crisis-center/...',
+    name: 'CrisisCenter',
+    component: CrisisCenterComponent,
+    useAsDefault: true
+  },
+  {path: '/heroes',   name: 'Heroes',     component: HeroListComponent},
+  {path: '/hero/:id', name: 'HeroDetail', component: HeroDetailComponent},
+  {path: '/disaster', name: 'Asteroid', redirectTo: ['CrisisCenter', 'CrisisDetail', {id:3}]}
+])
+*/
